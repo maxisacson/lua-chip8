2c4004532603a709b94a9b544046919fb7ac3b19
Max Isacson <max.isacson@physics.uu.se>
2016-05-22 15:08:14 +0200

    Implemented countdown of sound and delay timers.

--- main.lua
+++ main.lua
@@ -1,19 +1,31 @@
 cpu = require('cpu'):new()
 bit = require('bit')
 
-cpu:read_rom("roms/INVADERS")
+function love.load()
+    cpu:read_rom("roms/15PUZZLE")
+end
 
---cpu:dump_memory()
-
---cpu:run()
-
-function love.draw()
+function love.update(dt)
     local opcode = cpu:read_instruction()
     print("0x" .. bit.tohex(cpu.program_counter-2, 3) .. " opcode: 0x" .. bit.tohex(opcode, 4))
     cpu:run_instruction(opcode)
     cpu:print_state()
     --cpu:dump_screen()
 
+
+    -- The cpu and delay timers should decrease by 1 at a rate of 6Hz
+    -- TODO: ensure proper clock rate.
+    if cpu.delay_timer > 0 then
+        cpu.delay_timer = cpu.delay_timer - 1
+    end
+
+    if cpu.sound_timer > 0 then
+      cpu.sound_timer = cpu.sound_timer - 1
+    end
+
+end
+
+function love.draw()
     local scale_x = 16
     local scale_y = 16
 
@@ -29,5 +41,4 @@ function love.draw()
         end
         love.graphics.rectangle('fill', x*scale_x, y*scale_y, scale_x, scale_y)
     end
-
 end
