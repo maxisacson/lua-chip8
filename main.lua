42e2e0e4598be055920eb3b6b815f730341c7242
Max Isacson <max.isacson@physics.uu.se>
2016-05-23 12:52:56 +0200

    Started implementing input handling

--- main.lua
+++ main.lua
@@ -2,14 +2,14 @@ cpu = require('cpu'):new()
 bit = require('bit')
 
 function love.load()
-    cpu:read_rom("roms/15PUZZLE")
+    cpu:read_rom("roms/INVADERS")
 end
 
 function love.update(dt)
     local opcode = cpu:read_instruction()
-    print("0x" .. bit.tohex(cpu.program_counter-2, 3) .. " opcode: 0x" .. bit.tohex(opcode, 4))
+    --print("0x" .. bit.tohex(cpu.program_counter-2, 3) .. " opcode: 0x" .. bit.tohex(opcode, 4))
     cpu:run_instruction(opcode)
-    cpu:print_state()
+    --cpu:print_state()
     --cpu:dump_screen()
 
 
@@ -41,4 +41,78 @@ function love.draw()
         end
         love.graphics.rectangle('fill', x*scale_x, y*scale_y, scale_x, scale_y)
     end
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    print("--- Keypressed: ", key, scancode)
+    if key == '0' then
+        cpu:key_pressed(0x0)
+    elseif key == '1' then
+        cpu:key_pressed(0x1)
+    elseif key == '2' then
+        cpu:key_pressed(0x2)
+    elseif key == '3' then
+        cpu:key_pressed(0x3)
+    elseif key == '4' then
+        cpu:key_pressed(0x4)
+    elseif key == '5' then
+        cpu:key_pressed(0x5)
+    elseif key == '6' then
+        cpu:key_pressed(0x6)
+    elseif key == '7' then
+        cpu:key_pressed(0x7)
+    elseif key == '8' then
+        cpu:key_pressed(0x8)
+    elseif key == '9' then
+        cpu:key_pressed(0x9)
+    elseif key == 'a' then
+        cpu:key_pressed(0xa)
+    elseif key == 'b' then
+        cpu:key_pressed(0xb)
+    elseif key == 'c' then
+        cpu:key_pressed(0xc)
+    elseif key == 'd' then
+        cpu:key_pressed(0xd)
+    elseif key == 'e' then
+        cpu:key_pressed(0xe)
+    elseif key == 'f' then
+        cpu:key_pressed(0xf)
+    end
+end
+
+function love.keyreleased(key, scancode, isrepeat)
+    print("--- Keyreleased: ", key, scancode)
+    if key == '0' then
+        cpu:key_released(0x0)
+    elseif key == '1' then
+        cpu:key_released(0x1)
+    elseif key == '2' then
+        cpu:key_released(0x2)
+    elseif key == '3' then
+        cpu:key_released(0x3)
+    elseif key == '4' then
+        cpu:key_released(0x4)
+    elseif key == '5' then
+        cpu:key_released(0x5)
+    elseif key == '6' then
+        cpu:key_released(0x6)
+    elseif key == '7' then
+        cpu:key_released(0x7)
+    elseif key == '8' then
+        cpu:key_released(0x8)
+    elseif key == '9' then
+        cpu:key_released(0x9)
+    elseif key == 'a' then
+        cpu:key_released(0xa)
+    elseif key == 'b' then
+        cpu:key_released(0xb)
+    elseif key == 'c' then
+        cpu:key_released(0xc)
+    elseif key == 'd' then
+        cpu:key_released(0xd)
+    elseif key == 'e' then
+        cpu:key_released(0xe)
+    elseif key == 'f' then
+        cpu:key_released(0xf)
+    end  
 end
